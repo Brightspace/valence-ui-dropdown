87d70e66faafb3dc679d3ce9c1e150f47bd5eb34
Adrian Logan <adrian.logan@D2L.com>
2015-10-28 16:29:23 -0400

    Add in some more unit tests Fix up label for vui-dropdown-primary-active

--- test/unit/dropdownSpec.js
+++ test/unit/dropdownSpec.js
@@ -12,7 +12,51 @@
 				expect( document ).toHaveCssSelector( '.vui-dropdown-primary' );
 			});
 			it( 'defines a .vui-dropdown-dropdown-primary-active selector', function() {
-				expect( document ).toHaveCssSelector( '.vui-dropdown-dropdown-primary-active' );
+				expect( document ).toHaveCssSelector( '.vui-dropdown-primary-active' );
+			});
+			it( 'defines a .vui-dropdown-secondary selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-secondary' );
+			});
+			it( 'defines a .vui-dropdown-secondary-active selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-secondary-active' );
+			});
+			it( 'defines a .vui-dropdown-list selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-list' );
+			});
+			it( 'defines a .vui-dropdown-list-show selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-list-show' );
+			});
+			it( 'defines a .vui-dropdown-item selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-item' );
+			});
+			it( 'defines a .vui-dropdown-item-disabled selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-item-disabled' );
+			});
+			it( 'defines a .vui-dropdown-arrow-icon selector', function() {
+				expect( document ).toHaveCssSelector( '.vui-dropdown-arrow-icon::before' );
+			});
+		});
+
+		describe( 'menu list container', function() {
+			var node;
+
+			beforeEach( function() {
+				node = document.body.appendChild( document.createElement( 'div' ));
+				node.className = 'vui-dropdown-list-show';
+			});
+
+			afterEach( function() {
+				document.body.removeChild( node );
+			});
+
+			it( 'has border radius of 8px', function() {
+				expect( node ).toHaveBorderRadius( '8px' );
+			});
+			it( 'has text align center', function() {
+				expect( node ).toHaveTextAlign( 'center' );
+			});
+			it( 'has list style type of none', function() {
+				expect( node ).toHaveListStyleType( 'none' );
 			});
 
 		});
